b3177d77ba76f11edb7fd25377a9982277d49036
Perry-Anderson <perry425@revature.net>
2022-05-13 13:21:08 -0700

    Changed login function

--- src/main/java/com/revature/daos/UserDAO.java
+++ src/main/java/com/revature/daos/UserDAO.java
@@ -1,8 +1,14 @@
 package com.revature.daos;
 
+import java.util.List;
+
+import javax.persistence.Query;
+
 import org.hibernate.Session;
 
 import com.revature.utils.HibernateUtil;
+
+
 
 import com.revature.models.User;
 
@@ -12,7 +18,9 @@ public class UserDAO {
 	public User getUser(String username) {
 		System.out.print("Hello");
 		Session ses = HibernateUtil.getSession();
-		User user = ses.get(User.class, username);
+		Query q = ses.createQuery("FROM User u WHERE u.username = ?0");
+		q.setParameter(0, username);
+		User user = (User) q.getSingleResult();
 		HibernateUtil.closeSession();
 		return user;
 	}
@@ -23,6 +31,30 @@ public class UserDAO {
 		HibernateUtil.closeSession();
 		
 	}
-
+//public List<Movie> getMoviesByDirectorId(int id){
+//		
+//		Session ses = HibernateUtil.getSession();
+//		
+//		//create a query with a parameter that takes in the director id (given in the method argument)
+//		//"find every Movie which we'll call m, where the id of it's Director object = ?"
+//		//"m" is an alias (different name) that we gave to Movie, so help us make a cleaner query
+//		Query q = ses.createQuery("FROM Movie m WHERE m.director_fk.director_id = ?0");
+//		
+//		//set the ? to the id sent in the method call. This is how we PARAMETERIZE HQL
+//		q.setParameter(0, id);
+//		
+//		//create a List of Movies based on the results of the query
+//		List<Movie> movieList = q.getResultList();
+//		
+//		HibernateUtil.closeSession();
+//		
+//		return movieList;
+//		
+//		/*
+//		  I'd imagine this is a good template for user login
+//		  "FROM User u WHERE u.username = ?0 etc...."
+//		 */
+		
+	}
 	
-}
+
