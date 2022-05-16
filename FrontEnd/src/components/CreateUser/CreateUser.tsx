1ac35a13ec86d2e8d38406db5f7cbe5771e7d38b
Perry-Anderson <perry425@revature.net>
2022-05-16 14:26:48 -0700

    added some createuser

---
+++ FrontEnd/src/components/CreateUser/CreateUser.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
+
+export const CreateUser: React.FC<any> = () => {
+return(
+
+    <div className="login">
+
+        <div className="text-container">
+            <h1>Welcome to PokeMart!</h1>
+            <h3>Sign in to browse for a pokemanz</h3>
+
+    
+        </div>
+       
+    </div>
+)
+}
