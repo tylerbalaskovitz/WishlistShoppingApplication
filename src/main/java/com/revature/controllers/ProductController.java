d1542a69b444515a17eb8f430760eb950da71c03
Perry-Anderson <perry425@revature.net>
2022-05-19 16:28:25 -0700

    Working on Backend Wishlist

--- src/main/java/com/revature/controllers/ProductController.java
+++ src/main/java/com/revature/controllers/ProductController.java
@@ -1,10 +1,22 @@
 package com.revature.controllers;
 
+import com.google.gson.Gson;
+import com.revature.models.User;
+import com.revature.models.Wishlist;
+
 import io.javalin.http.Handler;
 
 public class ProductController {
+	ProductService ps = new ProductService();
 	public Handler addProductHandler = (ctx) ->{
-		System.out.println("Hi");
+		// the body String is created using the Handler's configuration's body
+				
+		String body = ctx.body();
+		Gson gson = new Gson();
+		Wishlist newWishlist = gson.fromJson(body, Wishlist.class);
+				
+		ps.addProductService(newWishlist);
+		ctx.status(202);
 		
 	};
 	
