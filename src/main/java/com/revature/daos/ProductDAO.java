ce194acadf0b9620c4070887933bcacd04d38c0d
tylerbalaskovitz <tylerbalaskovitz@yahoo.com>
2022-05-19 22:49:54 -0400

    the get user's wishlist is more or less complete on the DAO side. It searchable by id and is similar to the HelloHibernate demo Ben used to create his relationship.

--- src/main/java/com/revature/daos/ProductDAO.java
+++ src/main/java/com/revature/daos/ProductDAO.java
@@ -2,8 +2,11 @@ package com.revature.daos;
 
 import java.util.List;
 
+import javax.persistence.Query;
+
 import org.hibernate.Session;
 
+import com.revature.models.Movie;
 import com.revature.models.Wishlist;
 import com.revature.utils.HibernateUtil;
 
@@ -24,20 +27,30 @@ public class ProductDAO {
 		
 		
 	
-public List<Wishlist> getUserWishList(){
+		//List is used for getting a Wishlist by the id, so we can have individualized user information. What gets sent as the ID needs to be determined
+		//on the front end.
+public List<Wishlist> getUserWishList(int id){
 		
+		
+	
 		//open a Session object to connect to the DB
 		Session ses = HibernateUtil.getSession();
 		
-		//SELECT ALL movies using HQL instead of sessions methods.
-		List<Wishlist> userWishList = ses.createQuery("FROM Wishlist").list();
-		//we're selecting ALL records FROM the movies table. Remember, HQL uses Class names, not DB table names
 		
-		//close the session
+		//create a query with a parameter that takes in the user's id (given in the method argument)
+		//"find every product, p, where the id of it's Director object = ?"
+		//"m" is an alias (different name) that we gave to Movie, so help us make a cleaner query
+		Query q = ses.createQuery("FROM Wishlist w WHERE w.user_fk.user_id = ?0");
+		
+		//set the ? to the id sent in the method call. This is how we PARAMETERIZE HQL
+		q.setParameter(0, id);
+		
+		//create a List of Movies based on the results of the query
+		List<Wishlist> wishlist = q.getResultList();
+		
 		HibernateUtil.closeSession();
 		
-		//return the List of Movies
-		return userWishList;
+		return wishlist;
 		
 	}
 	
